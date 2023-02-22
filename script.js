e13281f9abb7ffb77e54a982341061ded1601b97
Pedro Freitas <pedro06bf@gmail.com>
2023-02-21 23:40:12 -0300

    artistas feitos

--- script.js
+++ script.js
@@ -47,7 +47,10 @@ class MobileNavbar {
     btn1.onclick = function(){
       var bio=document.querySelector(".artista :nth-child(2)");
       bio.classList.toggle("bio-active")
-      console.log("o")
+      var biobg=document.querySelector(".artista:nth-child(1) :nth-child(3)")
+      biobg.classList.toggle("bg-active")
+      btn1.classList.toggle("btn-active")
+
 
     }
 
@@ -55,22 +58,65 @@ class MobileNavbar {
     btn2.onclick = function(){
       var bio=document.querySelector(".artista:nth-child(2) :nth-child(2)");
       bio.classList.toggle("bio-active")
-      console.log(bio)
+      var biobg=document.querySelector(".artista:nth-child(2) :nth-child(3)")
+      biobg.classList.toggle("bg-active")
+      btn2.classList.toggle("btn-active")
     }
 
     const btn3 = document.querySelector(".artista:nth-child(3) :nth-child(1)");
     btn3.onclick = function(){
       var bio=document.querySelector(".artista:nth-child(3) :nth-child(2)");
       bio.classList.toggle("bio-active")
+      var biobg=document.querySelector(".artista:nth-child(3) :nth-child(3)")
+      biobg.classList.toggle("bg-active")
+      btn3.classList.toggle("btn-active")
     }
 
     const btn4 = document.querySelector(".artista:nth-child(4) :nth-child(1)");
     btn4.onclick = function(){
       var bio=document.querySelector(".artista:nth-child(4) :nth-child(2)");
       bio.classList.toggle("bio-active")
+      var biobg=document.querySelector(".artista:nth-child(4) :nth-child(3)")
+      biobg.classList.toggle("bg-active")
+      btn4.classList.toggle("btn-active")
     }
 
-    console.log(document.querySelector(".artista:nth-child(2) :nth-child(3)"))
+    const btn5 = document.querySelector(".artista:nth-child(5) :nth-child(1)");
+    btn5.onclick = function(){
+      var bio=document.querySelector(".artista:nth-child(5) :nth-child(2)");
+      bio.classList.toggle("bio-active")
+      var biobg=document.querySelector(".artista:nth-child(5) :nth-child(3)")
+      biobg.classList.toggle("bg-active")
+      btn5.classList.toggle("btn-active")
+    }
+
+    const btn6 = document.querySelector(".artista:nth-child(6) :nth-child(1)");
+    btn6.onclick = function(){
+      var bio=document.querySelector(".artista:nth-child(6) :nth-child(2)");
+      bio.classList.toggle("bio-active")
+      var biobg=document.querySelector(".artista:nth-child(6) :nth-child(3)")
+      biobg.classList.toggle("bg-active")
+      btn6.classList.toggle("btn-active")
+    }
+
+    const btn7 = document.querySelector(".artista:nth-child(7) :nth-child(1)");
+    btn7.onclick = function(){
+      var bio=document.querySelector(".artista:nth-child(7) :nth-child(2)");
+      bio.classList.toggle("bio-active")
+      var biobg=document.querySelector(".artista:nth-child(7) :nth-child(3)")
+      biobg.classList.toggle("bg-active")
+      btn7.classList.toggle("btn-active")
+    }
+
+    const btn8 = document.querySelector(".artista:nth-child(8) :nth-child(1)");
+    btn8.onclick = function(){
+      var bio=document.querySelector(".artista:nth-child(8) :nth-child(2)");
+      bio.classList.toggle("bio-active")
+      var biobg=document.querySelector(".artista:nth-child(8) :nth-child(3)")
+      biobg.classList.toggle("bg-active")
+      btn8.classList.toggle("btn-active")
+    }
+
 
     
 
